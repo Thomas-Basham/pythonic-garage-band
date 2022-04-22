dfcd798c247e59e31d375bbd709a04226f7f5154
Thomas Basham <Bashamtg@gmail.com>
2022-04-21 21:08:27 -0700

    passed all but last

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,24 +1,37 @@
-import abc
-from abc import ABC
 
 
-class Band(ABC):
-    def __init__(self, name="unknown", band_name=[], instruments=[], members = []):
+class Band:
+    instances = []
+
+    def __init__(self, name, members):
         self.name = name
-        Band.name = band_name
-        Band.instruments = instruments
-        Band.members = members
+        self.members = members
+        self.instances.append(self)
 
     @classmethod
     def get_instrument(cls):
         pass
 
+    def to_list(self):
+        pass
+
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+
+    def play_solos(self):
+        solos = []
+        for members in self.members:
+            solos.append(members.play_solo(self))
+        return solos
+
 
 class Musician(Band):
-    pass
+    def __init__(self, name):
+        self.name = name
 
 
-class Guitarist(Band):
+class Guitarist(Musician):
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
 
@@ -28,8 +41,12 @@ class Guitarist(Band):
     def get_instrument(self):
         return "guitar"
 
+    @staticmethod
+    def play_solo(self):
+        return "face melting guitar solo"
 
-class Bassist(Band):
+
+class Bassist(Musician):
     def __str__(self):
         return f"My name is {self.name} and I play bass"
 
@@ -39,8 +56,12 @@ class Bassist(Band):
     def get_instrument(self):
         return "bass"
 
+    @staticmethod
+    def play_solo(self):
+        return "bom bom buh bom"
 
-class Drummer(Band):
+
+class Drummer(Musician):
     def __str__(self):
         return f"My name is {self.name} and I play drums"
 
@@ -50,5 +71,6 @@ class Drummer(Band):
     def get_instrument(self):
         return "drums"
 
-
-
+    @staticmethod
+    def play_solo(self):
+        return "rattle boom crash"
--- tests/test_band.py
+++ tests/test_band.py
@@ -82,7 +82,6 @@ def test_instruments(one_band):
         assert member.get_instrument() == instruments[i]
 
 
-@pytest.mark.skip("todo")
 def test_individual_solos(one_band):
     for member in one_band.members:
         if member.get_instrument() == "guitar":
@@ -93,7 +92,6 @@ def test_individual_solos(one_band):
             assert member.play_solo() == "rattle boom crash"
 
 
-@pytest.mark.skip("todo")
 def test_band_members(one_band):
 
     assert len(one_band.members) == 3
@@ -111,7 +109,6 @@ def test_band_members(one_band):
     assert one_band.members[2].name == "Dave Grohl"
 
 
-@pytest.mark.skip("todo")
 def test_play_solos_for_whole_band(one_band):
     solos = one_band.play_solos()
     assert len(solos) == 3
@@ -120,7 +117,6 @@ def test_play_solos_for_whole_band(one_band):
     assert solos[2] == "rattle boom crash"
 
 
-@pytest.mark.skip("todo")
 def test_class_tracks_instances():
     assert Band.to_list() == []
     the_nobodies = Band("The Nobodies", [])
@@ -128,7 +124,6 @@ def test_class_tracks_instances():
     assert Band.instances[0] == the_nobodies
 
 
-@pytest.mark.skip("todo")
 def test_to_list():
     assert Band.to_list() == []
     the_nobodies = Band("The Nobodies", [])
